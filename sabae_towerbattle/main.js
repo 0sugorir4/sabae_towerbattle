6b6255634d773c5ceb1b02d39fce60847e133d21
0sugorir4 <46557440+0sugorir4@users.noreply.github.com>
2019-10-28 18:12:52 +0900

    sabae

---
+++ sabae_towerbattle/main.js
@@ -0,0 +1,56 @@
+﻿
+/***** Matter.js モジュール 初期設定（ここから） *****/
+
+var Engine = Matter.Engine,
+	Render = Matter.Render,
+	Runner = Matter.Runner,
+	Body = Matter.Body,
+	Events = Matter.Events,
+	Composite = Matter.Composite,
+	Composites = Matter.Composites,
+	Common = Matter.Common,
+	Constraint = Matter.Constraint,
+	MouseConstraint = Matter.MouseConstraint,
+	Mouse = Matter.Mouse,
+	World = Matter.World,
+	Bodies = Matter.Bodies;
+	Vector = Matter.Vector;
+
+/***** Matter.js モジュール 初期設定（ここまで） *****/
+
+//物理エンジンの生成
+var engine = Engine.create(),
+	world = engine.world;
+	
+//表示領域の生成
+var render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+		width: 800,
+		height: 600,
+		showVelocity: true
+	}
+});
+
+//表示領域を走らせる
+Render.run(render);
+
+//表示領域を走らせ続けるrunner関数をつくる
+var runner = Runner.create();
+
+//表示領域を走らせ続ける
+Runner.run(runner, engine);
+
+
+/***** ワールドに剛体を加える（ここから） *****/
+
+
+
+/***** ワールドに剛体を加える（ここまで） *****/
+
+/***** マウスのコントロール設定（ここから） *****/
+
+
+
+/***** マウスのコントロール設定（ここまで） *****/
